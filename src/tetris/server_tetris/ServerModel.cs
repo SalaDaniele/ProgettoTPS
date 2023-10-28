75fc3a6062acdb153768abadef67829652ce6104
ccarlo <carlo.cecco0@gmail.com>
2023-10-28 11:33:01 +0200

    Init controllers and models

---
+++ src/tetris/server_tetris/ServerModel.cs
@@ -0,0 +1,21 @@
+﻿public class ServerModel
+{
+    private char[][] gameBoard;
+    private char currentPlayer;
+
+    public ServerModel()
+    {
+        // Initialize the game board and other properties
+    }
+
+    public bool ProcessMove(int row, int col)
+    {
+        return false;
+        // Handle move validation, update the game board, and check for win/draw
+    }
+
+    //public char[][] GetGameBoard()
+    //{
+    //    // Provide the current game board state to the controller
+    //}
+}
