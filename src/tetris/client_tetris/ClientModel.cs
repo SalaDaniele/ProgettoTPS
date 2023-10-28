ab62e8a8098cc2c446f4879133ac64dafeb890d1
ccarlo <carlo.cecco0@gmail.com>
2023-10-28 11:51:28 +0200

    class updates

--- src/tetris/client_tetris/ClientModel.cs
+++ src/tetris/client_tetris/ClientModel.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace client_tetris
+﻿public class ClientModel
 {
-    internal class ClientModel
+    private char[][] gameBoard;
+    private char currentPlayer;
+    private bool isPlayerTurn;
+
+    public ClientModel()
     {
+        // Initialize game board and other properties
+    }
+
+    public void UpdateGameBoard(char[][] newBoard)
+    {
+        // Update the game board based on server updates
+    }
+
+    public void SetCurrentPlayer(char playerSymbol)
+    {
+        // Set the current player (X or O)
+    }
+
+    public bool IsPlayerTurn()
+    {
+        return true;
+        // Check if it's the player's turn
     }
 }
--- src/tetris/server_tetris/ServerController.cs
+++ src/tetris/server_tetris/ServerController.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace server_tetris
+﻿public class ServerController
 {
-    internal class ServerController
+    private ServerModel model;
+
+    public ServerController(ServerModel model)
     {
+        this.model = model;
+    }
+
+    public void StartGame()
+    {
+        // Start the game and handle the game loop
+        // Communicate with clients and process moves
+    }
+
+    public void EndGame()
+    {
+        // Handle game over conditions and inform clients
     }
 }
