81c75db75bd7868ab93cb2afbed44ea4bb69f4ed
ccarlo <carlo.cecco0@gmail.com>
2023-10-28 11:15:10 +0200

    init client, server and json message

---
+++ src/tetris/MATTILibrary/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+namespace MATTILibrary; // Modular Approach to Trasnmitting and Transforming Information
+
+
+public class Message
+{
+    public string MessageType { get; set; }
+    public string MessageData { get; set; }
+
+    public Message(string messageType, string messageData)
+    {
+        MessageType = messageType;
+        MessageData = messageData;
+    }
+
+    public string ToJson()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+
+    public static Message FromJson(string jsonString)
+    {
+        return JsonSerializer.Deserialize<Message>(jsonString);
+    }
+}
---
+++ src/tetris/client_tetris/Program.cs
@@ -0,0 +1,41 @@
+﻿using MATTILibrary;
+using System.Net;
+using System.Net.Sockets;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var ipe = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+        var socket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        socket.Connect(ipe);
+
+        Console.WriteLine("Connected");
+
+        var writer = new StreamWriter(new NetworkStream(socket));
+        writer.AutoFlush = true;
+
+        var reader = new StreamReader(new NetworkStream(socket));
+
+
+        while (true)
+        {
+            //Console.WriteLine("Enter message to send:");
+            //var message = Console.ReadLine();
+            //writer.WriteLine(message);
+
+            Message clientMessage = new Message("MOVE", "1 1");
+            string jsonMessage = clientMessage.ToJson();
+            writer.WriteLine(jsonMessage);
+
+            var responseMessage = reader.ReadLine();
+            Console.WriteLine("Message from server: " + responseMessage);
+
+            Console.ReadLine();
+        }
+
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+}
